6e817a4ac96d1fb32dc2cb969704fb6397f776f9
Oscar Koeroo <okoeroo@gmail.com>
2024-01-06 21:39:50 +0100

    Kick old code

--- huntdns.py
+++ huntdns.py
@@ -11,34 +11,6 @@ def huntdns_fieldnames() -> list[str]:
     return huntdns_get_defaults()
 
 
-# def query_dns(fqdn, r_type) -> str:
-#     try:
-#         answers = dns.resolver.resolve(fqdn, r_type)
-#         rdatas = ", ".join([str(rdata) for rdata in answers])
-        
-#         return rdatas
-
-#     except dns.resolver.NoAnswer as err:
-#         print(f"NoAnswer: {err}")
-#         return "Error: NoAnswer"
-#     except dns.resolver.NoNameservers as err:
-#         print(f"NoNameservers: {err}")
-#         return "Error: NoNameservers"
-#     except dns.resolver.NXDOMAIN as err:
-#         print(f"NXDOMAIN: {err}")
-#         return "Error: NXDOMAIN"
-#     except Exception as err:
-#         print(f"Unexpected ({fqdn}:{r_type}) {err}, {type(err)}")
-#         return f"Error: Unexpected: {err} :: {type(err)}"
-
-
-# def huntdns(prey, target_r_types, target) -> None:
-#     for r_type in target_r_types:
-#         prey[r_type] = query_dns(target, r_type)
-
-#     return prey
-
-
 async def query_dns(hostname, r_type='A'):
     loop = asyncio.get_event_loop()
     

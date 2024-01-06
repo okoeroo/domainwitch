9ea0866c5e26c86441d3b64872495b00d7562249
Oscar Koeroo <okoeroo@gmail.com>
2024-01-06 17:13:14 +0100

    Reporting timeouts and errors into report

--- huntredirect.py
+++ huntredirect.py
@@ -1,11 +1,12 @@
+from httpx import Response
 from supporthttp import build_url, fetch_http
 
 
 # Returns a list of tuples with url, http status_code, location/None
 def analyse_http(url: str):
     r = fetch_http(url, timeout=4)
-    if r is None:
-        return None
+    if type(r) is not Response:
+        return r
 
     if r.status_code >= 300 and r.status_code < 400:
         # tup = analyse_http(r.headers['Location'])
@@ -20,11 +21,14 @@ def huntredirect(prey: dict, target: str):
     for port, scheme in port_list.items():
         if prey[f"port_{port}"]:
             url = build_url(scheme=scheme, host=target, port=port)
-            tup = analyse_http(url)
-            if tup is None:
+            res = analyse_http(url)
+
+            if res is None:
                 prey[f"redirect_http_{port}"] = None
+            elif type(res) is str:
+                prey[f"redirect_http_{port}"] = res
             else:
-                res_txt = f"url: {tup[0]} :: status: {tup[1]} -> location: {tup[2]}"
+                res_txt = f"url: {res[0]} :: status: {res[1]} -> location: {res[2]}"
                 prey[f"redirect_http_{port}"] = res_txt
 
     return prey
